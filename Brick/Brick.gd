f6ab528384d06ff90b0a825ffbacf0deb63a73dc
Sonnykimee <70347693+Sonnykimee@users.noreply.github.com>
2020-11-03 19:31:53 -0500

    Re-upload #1

--- Brick/Brick.gd
+++ Brick/Brick.gd
@@ -3,22 +3,32 @@ extends KinematicBody2D
 onready var HUD = get_node("/root/Game/HUD")
 var row = 0
 var col = 0
+var dying = false
 
 func _ready():
 	HUD.connect("changed",self,"_on_HUD_changed")
 	update_color()
 
+
+func _process(_delta):
+	if dying and not $Particles2D.emitting:
+		queue_free()
+
+
 func update_color():
 	if HUD.color_blocks:
-		pass
+		if row == 0:
+			$Color.color = Color8(224, 49, 49)
+		elif row == 1:
+			$Color.color = Color8(253, 126, 20)
 	else:
-		pass
+		$Color.color = Color(1,1,1,1)
+
 
 func emit_particle(pos):
 	if HUD.particle_blocks:
-		pass
-	else:
-		pass
+		$Particles2D.global_position = pos
+		$Particles2D.emitting = true
 	
 	
 func _on_HUD_changed():
@@ -26,4 +36,7 @@ func _on_HUD_changed():
 
 
 func die():
-	queue_free()
+	dying = true
+	$Color.color.a = 0
+	collision_layer = 0
+	collision_mask = 0
